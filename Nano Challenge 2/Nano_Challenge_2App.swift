daa97a3ba2b92036a481510bebaf073968a3fa7b
Felicia <fprisqilla22@bsd.idserve.net>
2022-07-21 15:36:25 +0700

    Initial Commit

---
+++ Nano Challenge 2/Nano_Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge_2App.swift
+//  Nano Challenge 2
+//
+//  Created by Felicia Prisqilla on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
